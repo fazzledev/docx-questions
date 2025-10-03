2f3959aa87beda3fcf904db57f2fae1db4912250
Syed Fazil Basheer <hey@fazzle.dev>
2025-10-03 11:03:03 +0530

    Initial the gem repo

---
+++ lib/docx/questions.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "questions/version"
+
+module Docx
+  module Questions
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/docx/questions/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Docx
+  module Questions
+    VERSION = "0.1.0"
+  end
+end
